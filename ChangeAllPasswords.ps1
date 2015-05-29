e5141e2ae8e8502e1d4c2c80e23548df2c8a2b5a
Derek Gusoff <dgusoff1@gmail.com>
2015-05-29 15:45:38 -0400

    Create ChangeAllPasswords.ps1

---
+++ ChangeAllPasswords.ps1
@@ -0,0 +1,34 @@
+##ChangeAllPasswords.ps1
+# changes the passwords to all accounts in a Microsoft Office Demo "Contoso" environment to a new specified value
+# Prerequisite: Azure Active Directory Module for Windows PowerShell
+# https://technet.microsoft.com/en-us/library/jj151815.aspx
+# https://www.microsoft.com/en-us/download/details.aspx?id=41950
+# Derek Gusoff, May 29, 2015
+
+
+$msolcred = get-credential
+connect-msolservice -credential $msolcred
+
+
+Set-MsolUserPassword -UserPrincipalName garthf@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName mollyd@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName alexd@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName allieb@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName annew@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName azizh@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName belindan@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName bonniek@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName brianj@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName davidl@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName denisd@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName dorenap@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName fabricec@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName garretv@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName janets@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName juliani@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName robinc@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName roby@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName sarad@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName tonyk@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName zrinkam@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
+Set-MsolUserPassword -UserPrincipalName admin@mytenant.onmicrosoft.com -NewPassword pass@word1 -ForceChangePassword $false
