3f8f4980f1f28f14824951d6503f2dd4790fa6c5
Derek Gusoff <dgusoff1@gmail.com>
2018-06-22 22:45:36 -0400

    quickly create a bunch of classic sp online site collections

---
+++ BatchCreateSites.ps1
@@ -0,0 +1,15 @@
+﻿# create a bunch of SP Online sites
+
+$tenant = "m365x234372"
+$userName = "admin@M365x234372.onmicrosoft.com"
+$seed = "groupsTest"
+$numSites = 10
+$template = "BLANKINTERNETCONTAINER#0"
+$quota = 1024
+
+Connect-SPOService
+
+for ($i = 0; $i -lt $numSites; $i++){
+   $siteTitle = $seed + $i
+   New-SPOSite -Url "https://$tenant.sharepoint.com/sites/$siteTitle" -Owner $userName -Title "$siteTitle" -Template $template -StorageQuota $quota -NoWait
+}
