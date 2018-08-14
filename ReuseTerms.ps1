c3aee9d74e85ea3e9da00a9ce598f8868801d469
Derek Gusoff <dgusoff1@gmail.com>
2018-08-13 22:39:35 -0400

    add comments

--- ReuseTerms.ps1
+++ ReuseTerms.ps1
@@ -1,3 +1,7 @@
+# ReuseTerms
+# Derek Gusoff. https://github.com/dgusoff
+# shows how to reuse a term and/or reuse with pinning
+
 if($cred -eq $null){
  $cred = Get-Credential
 }
@@ -13,5 +17,6 @@ $sourceTermSet.ReuseTerm($pinnedTerm, $true)
 
 # option 2: reuse with pinning
 $sourceTermSet.ReuseTermWithPinning($pinnedTerm)
+
 (Get-PnPContext).Load($pinnedTerm)
 Invoke-PnPQuery
